03e4ba4923477031ce844b50ad4b372b57641ac2
filija <filija.jakub@gmail.com>
2016-05-03 22:20:30 +0200

    some edit

--- src/ija/ija2015/actions/StartNewGame.java
+++ src/ija/ija2015/actions/StartNewGame.java
@@ -3,13 +3,14 @@ package ija.ija2015.actions;
 
 import ija.ija2015.gui.NewGameMenu;
 import javax.swing.JFrame;
-import java.awt.event.*;
 
-public class StartNewGame{
-    
+public class StartNewGame {
+    private NewGameMenu newGame;
     public void startNewGame(JFrame frame) {
         frame.setVisible(false);
         frame.dispose();
-        new NewGameMenu();
+        newGame=new NewGameMenu();
+        
+        
     }
 }
