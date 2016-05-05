542f8a7c29045e01ed52d1659a79d56cda95c91f
filija <filija.jakub@gmail.com>
2016-05-05 12:59:28 +0200

    some edit

--- src/ija/ija2015/gui/FieldGUI.java
+++ src/ija/ija2015/gui/FieldGUI.java
@@ -3,14 +3,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
+import ija.ija2015.gui.CreatBoard;
 /**
  * 
  * @author Jakub Filipek
  *	Trida Definujici graficke policko desky
  */
 
-public class FieldGUI extends JPanel implements MouseListener{
+public class FieldGUI extends JPanel{
 	ImageIcon white;
 	ImageIcon black;
 	JLabel diskerW;
@@ -39,7 +39,7 @@ public class FieldGUI extends JPanel implements MouseListener{
 	 * Funkce pro vlozeni obrazku kamene na pole
 	 */
 	public void putImgDisk(colorDisk color){
-		        
+		     
 		if(colorDisk.WHITE==color)
 		{
 			diskerB.setVisible(false);
@@ -48,50 +48,12 @@ public class FieldGUI extends JPanel implements MouseListener{
 			diskerW.setVisible(true);
 		}
 		else{
-			if(diskerW.isVisible()){
-				diskerW.remove(this);
-				this.add(diskerB, BorderLayout.CENTER);
-				diskerB.setVisible(true);
-			}
-			else{
-				diskerW.setVisible(false);
-				diskerB.setVisible(true);
-			}						
+			diskerW.setVisible(false);	
+			diskerB.remove(this);
+			this.add(diskerB, BorderLayout.CENTER);
+			diskerB.setVisible(true);			
 		}
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		if(this.playerOnMove==colorDisk.WHITE)
-			this.putImgDisk(colorDisk.WHITE);
-		else
-			this.putImgDisk(colorDisk.BLACK);
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
